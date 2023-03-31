aaa07aedacd6e02eedd085f96e6dfa629500d558
Aleksandr-Alekseenko-23 <105487472+Aleksandr-Alekseenko-23@users.noreply.github.com>
2023-03-31 20:16:29 +0000

    Deploying to gh-pages from @ Aleksandr-Alekseenko-23/work-portfolio@82ff42cef91733384e9878e0611a8c48d781b32e 🚀

---
+++ dist/components/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+var App = function () {
+    return (0, jsx_runtime_1.jsx)("div", { children: "React homework template" });
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
